990c5e4392be56afb51542460ff5e90cf2cf7683
emmanuelU17 <emmanueluluabuike@gmail.com>
2023-12-20 19:23:25 -0500

    removed flutterwave

---
+++ src/app/store-front/payment/payment/payment.component.ts
@@ -0,0 +1,14 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-payment',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PaymentComponent {
+
+}
