1b0c515f2d4fc8f44bb37d780572cebcb75246ff
emmanuelU17 <emmanueluluabuike@gmail.com>
2024-03-15 09:24:55 -0400

    temporarily removing paystack to validate if it is causing error

--- src/app/store-front/order/order-routes.ts
+++ src/app/store-front/order/order-routes.ts
@@ -1,13 +1,13 @@
 import {Routes} from "@angular/router";
 import {checkoutGuard} from "./checkout/checkout.guard";
 import {importProvidersFrom} from "@angular/core";
-import {Angular4PaystackModule} from "angular4-paystack";
+// import {Angular4PaystackModule} from "angular4-paystack";
 
 export const route: Routes = [
   {
     path: '',
     loadComponent: () => import('./payment/payment.component').then(m => m.PaymentComponent),
-    providers: [importProvidersFrom(Angular4PaystackModule.forRoot('pk_test_xxxxxxxxxxxxxxxxxxxxxxxx'))]
+    // providers: [importProvidersFrom(Angular4PaystackModule.forRoot('pk_test_xxxxxxxxxxxxxxxxxxxxxxxx'))]
   },
   {
     path: 'cart',
